d1dc3fd0002e63553647d12d9139237aa758c759
tenbite-daniel <tenbitedaniel60@gmail.com>
2026-03-13 02:09:36 +0300

    chore: Project Setup
    - Configured NativeWind/TailWindCSS
    - Removed default assets and added project assets
    - Added fonts and importd them to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,39 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { LogBox } from "react-native";
+import "react-native-reanimated";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+LogBox.ignoreLogs(["Clerk:"]);
 
 export default function RootLayout() {
-  return <Stack />;
+	const [loaded] = useFonts({
+		"Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+		"Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+		"Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+		"Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+		"Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+		Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+		"Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+	});
+
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
+
+	if (!loaded) {
+		return null;
+	}
+
+	return (
+		<Stack>
+			<Stack.Screen name="index" options={{ headerShown: false }} />
+		</Stack>
+	);
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+	return (
+		<View className="flex-1 justify-center align-center">
+			<Text className="text-5xl text-blue-500 font-bold">Aura</Text>
+		</View>
+	);
 }
